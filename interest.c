364a4e7e646b3b897b9dc4dd43e613e02d865bd0
Bajpaisaumya <134991597+Bajpaisaumya@users.noreply.github.com>
2023-06-11 00:56:20 +0530

    Create interest.c

---
+++ interest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float p,r,t,s;
+    printf("input the valur of p,r,t");
+    scanf("%f%f%f",&p,&r,&t);
+    s=p*t*r/100;
+    printf("simple interesyt=%f",s);
+    return 0;
+}
